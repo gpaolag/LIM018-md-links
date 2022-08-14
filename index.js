52f36ba95ff99e5563dc88bb89f0850b830c15bd
Paola <glenypaola.7@gmail.com>
2022-08-14 01:44:03 -0500

    primer prueba

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+
+function mdLinks(path, options) {
+  if (options == true) {
+    const datos = getData(path);
+    console.log(typeof datos);
+    // const urlRevision = validate(datos);
+    // console.log("esta: " + urlRevision);
+    return datos;
+  };
+}
+
+const getData = (path) => {
+  const promiseData = fs.readFile(path, 'utf8', (error, datos) => {
+    if (error) throw error;
+    console.log('valor: ' + datos);
+  });
+  return promiseData;
+}
+
+const validate = (url) => {
+  let responseValidate = '';
+  const datosValidate = fetch(url).then(Response => {
+    console.log('El status de ese link es: ' + Response.status);
+    console.log('Esta activo: ' + Response.ok);
+    return responseValidate = `${Response.status}` + ' ' + `${Response.ok}`;
+  });
+  return datosValidate;
+}
+
+// archivoPrueba = "prueba.md";
+
+// fs.readFile(archivoPrueba, 'utf8', (error, datos) => {
+//   if (error) throw error;
+//   console.log("El contenido es: ", datos);
+// });
+
+mdLinks('D:\\Laboratoria\\Cuarto proyecto\\LIM018-md-links\\prueba2.md', true);
+validate('https://github.com/');
+//const dato1 = mdLinks('D:\\Laboratoria\\Cuarto proyecto\\LIM018-md-links\\prueba2.md', true);
+//const dato2 = validate('https://github.com/');
+// const datos1 = getData('D:\\Laboratoria\\Cuarto proyecto\\LIM018-md-links\\prueba2.md');
+// setTimeout(function () {
+//   console.log(datos1);
+// }, 2000);
 module.exports = () => {
   // ...
 };
